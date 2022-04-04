4ffcee286d942343ca785eabde97564769ed4ad0
muril <murilra@hotmail.com>
2022-04-04 11:59:42 -0300

    initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            icon:const Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso(Kg)",
+                labelStyle: TextStyle(color: Colors.green)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          )
+        ],
+      ),
+    );
+  }
+}
